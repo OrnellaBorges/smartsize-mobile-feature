304576d4e1161fc26b95a961e6296b82eea33ab5
Ornella B <borges.ornella@gmail.com>
2024-07-23 12:04:45 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <main className="main">
+        <header className="header">
+          <h1 className="title title-1">Size Guide</h1>
+        </header>
+      </main>
+    </div>
+  );
+}
+
+export default App;
