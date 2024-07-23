f5c465712c9e9ce4de5ac7a010cd0ce2e14ccd65
Ornella B <borges.ornella@gmail.com>
2024-07-23 13:24:36 +0200

    improving select css appearance

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import "./App.css";
+import "./reset.css";
 
 function App() {
+  const options = [
+    { value: "apple", label: "Pomme" },
+    { value: "banana", label: "Banane" },
+    { value: "orange", label: "Orange" },
+  ];
+
   return (
     <div className="App">
       <main className="main">
         <header className="header">
           <h1 className="title title-1">Size Guide</h1>
+          <div className="select-wrapper">
+            <label className="label" htmlFor="">
+              Options
+              <select
+                defaultValue="apple"
+                className="select"
+                name="selectedFruit"
+              >
+                <option value="apple">A</option>
+                <option value="banana">B</option>
+                <option value="orange">Orange</option>
+                <option value="orange">Orangefvdsgs</option>
+                <option value="orange">treslonguephraseeeeeeeee</option>
+              </select>
+            </label>
+          </div>
         </header>
       </main>
     </div>
